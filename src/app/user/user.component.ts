6544e9030ec716e31c5addd29184799502583e2e
braedengoodwin <braedenagoodwin@hotmail.com>
2025-03-06 18:21:14 -0600

    got through lecture 42

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// this is a type alias
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -11,16 +22,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // selected user is technically a property of the class UserComponent, so call it a PROPERTY
-  selectedUser = DUMMY_USERS[randomIndex];
+  // the '!' tells typescript that we know that avatar WILL DEFINITELY be assigned a value eventually
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
 
   // this getter is referenced as a 'property' in the user.component.html file
   get imagePath() {
-    return '/assets/users/' + this.selectedUser.avatar;
+    return '/assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.select.emit(this.user.id);
   }
 }
